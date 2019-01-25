50bb5f6550aba6143af9acc7c9310e1f24b00856
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-01-25 17:46:44 +0900

    use pip internally to install mpi4py from its github repository

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import sys
-import setuptools as st
+from pip._internal import main as pip_main
+from setuptools import setup, find_packages
+
 
 try:
   from lammps import lammps, PyLammps
@@ -9,23 +11,35 @@ except ImportError:
   print("and set a path to the library directory.")
   sys.exit(1)
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-st.setup(
-    name="wapylmp",
-    version="0.3.1a",
-    description="Extension of Atomic Simulation Environment for LAMMPS",
-    author="Takayuki Kobayashi",
-    author_email="iris.takayuki@gmail.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/irisTa56/wapylmp.git",
-    install_requires=["mpi4py"],
-    dependency_links=['git+https://github.com/mpi4py/mpi4py.git#egg=mpi4py'],
-    packages=st.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-    ),
+try:
+  import mpi4py
+except ImportError:
+  print("Get and build mpi4py...")
+
+  pip_command = ["install"]
+  if "--user" in sys.argv:
+    pip_command.append("--user")
+  pip_command.append("git+https://github.com/mpi4py/mpi4py.git")
+
+  pip_main(pip_command)
+
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name="wapylmp",
+  version="0.3.1a",
+  description="Extension of Atomic Simulation Environment for LAMMPS",
+  author="Takayuki Kobayashi",
+  author_email="iris.takayuki@gmail.com",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/irisTa56/wapylmp.git",
+  packages=find_packages(),
+  classifiers=(
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+  ),
 )
